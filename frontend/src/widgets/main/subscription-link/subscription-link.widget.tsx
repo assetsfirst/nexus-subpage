8596a720d6d2d725ea4fa20d0cf798e355659f0c
pods <pods611@gmail.com>
2025-11-27 21:04:39 +0300

    fix: add small improvements

--- frontend/src/widgets/main/subscription-link/subscription-link.widget.tsx
+++ frontend/src/widgets/main/subscription-link/subscription-link.widget.tsx
@@ -1,11 +1,5 @@
-import {
-    IconBrandDiscord,
-    IconBrandTelegram,
-    IconBrandVk,
-    IconLink,
-    IconMessageChatbot
-} from '@tabler/icons-react'
-import { ActionIcon, Button, Group, Image, Stack, Text } from '@mantine/core'
+import { IconBrandDiscord, IconBrandTelegram, IconBrandVk, IconLink, IconMessageChatbot } from '@tabler/icons-react'
+import { ActionIcon, Button, Group, Image, Stack, Text, Tooltip } from '@mantine/core'
 import { notifications } from '@mantine/notifications'
 import { useTranslation } from 'react-i18next'
 import { useClipboard } from '@mantine/hooks'
@@ -24,14 +18,14 @@ export const SubscriptionLinkWidget = ({ supportUrl }: { supportUrl?: string }) 
 
     const subscriptionUrl = constructSubscriptionUrl(
         window.location.href,
-        subscription.user.shortUuid
+        subscription.user.shortUuid,
     )
 
     const handleCopy = () => {
         notifications.show({
             title: t('subscription-link.widget.link-copied'),
             message: t('subscription-link.widget.link-copied-to-clipboard'),
-            color: 'teal'
+            color: 'teal',
         })
         clipboard.copy(subscriptionUrl)
     }
@@ -40,11 +34,11 @@ export const SubscriptionLinkWidget = ({ supportUrl }: { supportUrl?: string }) 
         const iconConfig = {
             't.me': { icon: IconBrandTelegram, color: '#0088cc' },
             'discord.com': { icon: IconBrandDiscord, color: '#5865F2' },
-            'vk.com': { icon: IconBrandVk, color: '#0077FF' }
+            'vk.com': { icon: IconBrandVk, color: '#0077FF' },
         }
 
         const matchedPlatform = Object.entries(iconConfig).find(([domain]) =>
-            supportUrl.includes(domain)
+            supportUrl.includes(domain),
         )
 
         const { icon: Icon, color } = matchedPlatform
@@ -68,42 +62,44 @@ export const SubscriptionLinkWidget = ({ supportUrl }: { supportUrl?: string }) 
 
     return (
         <Group gap="xs">
-            <ActionIcon
-                onClick={() => {
-                    const subscriptionQrCode = renderSVG(subscriptionUrl, {
-                        whiteColor: '#161B22',
-                        blackColor: '#3CC9DB'
-                    })
+            <Tooltip label={t('subscription-link.widget.get-link')}>
+                <ActionIcon
+                    onClick={() => {
+                        const subscriptionQrCode = renderSVG(subscriptionUrl, {
+                            whiteColor: '#161B22',
+                            blackColor: '#3CC9DB',
+                        })
 
-                    modals.open({
-                        centered: true,
-                        title: t('subscription-link.widget.get-link'),
-                        children: (
-                            <>
-                                <Stack align="center">
-                                    <Image
-                                        src={`data:image/svg+xml;utf8,${encodeURIComponent(subscriptionQrCode)}`}
-                                    />
-                                    <Text fw={600} size="lg" ta="center">
-                                        {t('subscription-link.widget.scan-qr-code')}
-                                    </Text>
-                                    <Text c="dimmed" size="sm" ta="center">
-                                        {t('subscription-link.widget.line-1')}
-                                    </Text>
+                        modals.open({
+                            centered: true,
+                            title: t('subscription-link.widget.get-link'),
+                            children: (
+                                <>
+                                    <Stack align="center">
+                                        <Image
+                                            src={`data:image/svg+xml;utf8,${encodeURIComponent(subscriptionQrCode)}`}
+                                        />
+                                        <Text fw={600} size="lg" ta="center">
+                                            {t('subscription-link.widget.scan-qr-code')}
+                                        </Text>
+                                        <Text c="dimmed" size="sm" ta="center">
+                                            {t('subscription-link.widget.line-1')}
+                                        </Text>
 
-                                    <Button fullWidth onClick={handleCopy} variant="filled">
-                                        {t('subscription-link.widget.copy-link')}
-                                    </Button>
-                                </Stack>
-                            </>
-                        )
-                    })
-                }}
-                size="xl"
-                variant="default"
-            >
-                <IconLink />
-            </ActionIcon>
+                                        <Button fullWidth onClick={handleCopy} variant="filled">
+                                            {t('subscription-link.widget.copy-link')}
+                                        </Button>
+                                    </Stack>
+                                </>
+                            ),
+                        })
+                    }}
+                    size="xl"
+                    variant="default"
+                >
+                    <IconLink />
+                </ActionIcon>
+            </Tooltip>
             {supportUrl && renderSupportLink(supportUrl)}
         </Group>
     )
